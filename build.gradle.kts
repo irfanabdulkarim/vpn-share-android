b62067d0b3d8057d4a418a9b9e50d70d4f187642
root <root@localhost.localdomain>
2025-11-12 14:26:27 +0000

    Fix: Simplify build.gradle.kts with plugin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 // Top-level build file
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
